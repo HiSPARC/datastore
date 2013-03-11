cb5aaa3801925b72b069efabd6ead2cbf9bf1065
Arne de Laat <adelaat@nikhef.nl>
2013-03-11 23:03:54 +0100

    PEP8 and english
    Remove to many spaces
    Translate some comments and docstrings to english

--- wsgi/rcodes.py
+++ wsgi/rcodes.py
@@ -1,20 +1,20 @@
-""" 
+"""
 Return codes.
-Er zijn vier soorten return codes:
+There are four types of return codes:
 1. OK (100)
 2. putEvent errors (20*)
 3. getEvent errors (30*)
 3. Internal server error (40*)
 """
 
-RC_OK               = 100 # OK, alles is gelukt.
+RC_OK = 100 # OK, Everything went fine.
 
 # putEvent
-RC_PE_INV_INPUT     = 201 # Wrong input
-RC_PE_INV_UPCODE    = 202 # Wrong upload code
-RC_PE_INV_AUTHCODE  = 203 # Wrong password
-RC_PE_EVENT_EXISTS  = 204 # Event bestaat al in de database.
-RC_PE_TRANS_FAILED  = 205 # Transaction failed
+RC_PE_INV_INPUT = 201 # Wrong input
+RC_PE_INV_UPCODE = 202 # Wrong upload code
+RC_PE_INV_AUTHCODE = 203 # Wrong password
+RC_PE_EVENT_EXISTS = 204 # Event already exists in de database.
+RC_PE_TRANS_FAILED = 205 # Transaction failed
 RC_PE_INV_STATIONID = 206 # Station Unknown
 RC_PE_INV_EVENTTYPE = 207 # Wrong event type
 
@@ -22,4 +22,4 @@ RC_PE_INV_EVENTTYPE = 207 # Wrong event type
 
 # Internal server errors.
 RC_ISE_INV_POSTDATA = 400 # HTTP Bad Request
-RC_ISE_DB_CONNECT_FAILED = 404 # Database onbereikbaar.
+RC_ISE_DB_CONNECT_FAILED = 404 # Database unreachable.
