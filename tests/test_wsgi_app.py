149bcbff466843529cedb831e5d2ea913532cc67
Tom Kooij <tomkooij@tomkooij.nl>
2017-02-25 18:59:13 +0100

    Refactor test_wsgi_app using WebTest
    Use WebTest to test the WSGI app without uWSGI.

--- tests/test_wsgi_app.py
+++ tests/test_wsgi_app.py
@@ -1,111 +1,121 @@
 import unittest
 import requests
+import functools
 import hashlib
 import pickle
 import shutil
+import sys
+from io import StringIO
+
+from webtest import TestApp
 
 
-uWSGI_URL = 'http://localhost:80'
+# configuration:
+CONFIGFILE = 'test_data/config.ini'
+WSGI_APP_PATH = '../wsgi'
+
+STATION_ID = 99
+PASSWORD = 'fake_station'
+
 EVENTPY2 = 'test_data/incoming_http/py2event'
 EVENTPY3 = 'test_data/incoming_http/py3event'
 EVENTSUS = 'test_data/incoming_http/suspicious_event'
 
 
-class Uploader(object):
+def import_wsgi_app():
+    """import the WSGI application"""
+    sys.path.append(WSGI_APP_PATH)
+    import wsgi_app
+    return functools.partial(wsgi_app.application, configfile=CONFIGFILE)
 
-    def __init__(self, url, sn, password):
-        self.url = url
-        self.station_id = sn
-        self.password = password
 
-    def upload_data(self, pickled_data, checksum=None):
-        """Upload event data to wsgi-app.
-
-        :param pickled_data: pickled event data (bytestring).
-        :param checksum: optional checksum to send.
-
-        returns the rcode or raises UploadError.
-
-        """
-        if checksum is None:
-            checksum = hashlib.md5(pickled_data).hexdigest()
-
-        payload = {'station_id': self.station_id,
-                   'password': self.password, 'data': pickled_data.decode('latin-1'),
-                   'checksum': checksum}
-        try:
-            r = requests.post(self.url, data=payload, timeout=10)
-            r.raise_for_status()
-        except (ConnectionError) as exc:
-            raise UploadError(str(exc))
-        else:
-            return r.text
+def get_wsgi_app(wsgi_app=import_wsgi_app()):
+    """return the WSGI application"""
+    return wsgi_app
 
 
 class TestWsgiApp(unittest.TestCase):
 
-    url = uWSGI_URL
-    uploader = Uploader(url, 99, 'fake_station')
-    pickled_eventlist_py2 = open(EVENTPY2, 'rb').read()
-    pickled_eventlist_py3 = open(EVENTPY3, 'rb').read()
-    suspicious_eventlist = open(EVENTSUS, 'rb').read()
-
     def setUp(self):
-        # clean fake_datstore
-        pass
- 
+        self.station_id = STATION_ID
+        self.password = PASSWORD
+        self.app = TestApp(get_wsgi_app())
+
     def test_invalid_post_data(self):
-        r = requests.post(self.url, data={}, timeout=10)
-        self.assertEqual(r.text, '400') # invalid post data
+        resp = self.app.post('/', {})
+        self.assertEqual(resp.body, b'400') # invalid post data
 
     def test_unpickling_error(self):
         broken_pickle = b'aaaaa'
-        checksum = hashlib.md5(broken_pickle).hexdigest()
-        r_text =  self.uploader.upload_data(broken_pickle, checksum=checksum)
-        self.assertEqual(r_text, '208') # unpickle error
+        resp = self.upload(broken_pickle)
+        self.assertEqual(resp, b'208') # unpickle error
 
     def test_invalid_checksum(self):
-        r_text = self.uploader.upload_data(self.pickled_eventlist_py2,
-                                           checksum=b'invalid')
-        self.assertEqual(r_text, '201') # input error
+        event_list = self.read_pickle(EVENTPY2)
+        resp = self.upload(event_list, checksum=b'invalid')
+        self.assertEqual(resp, b'201') # input error
 
     def test_invalid_station_id(self):
-        uploader = Uploader(self.url, 0, 'password')
-        r_text = uploader.upload_data(self.pickled_eventlist_py2)
-        self.assertEqual(r_text, '206') # invalid station_id
+        event_list = self.read_pickle(EVENTPY2)
+        self.station_id = 0 # invalid station
+        resp = self.upload(event_list)
+        self.assertEqual(resp, b'206') # invalid station id
 
     def test_invalid_password(self):
-        uploader = Uploader(self.url, 99, 'wrong_password')
-        r_text = uploader.upload_data(self.pickled_eventlist_py2)
-        self.assertEqual(r_text, '203') # wrong password
+        event_list = self.read_pickle(EVENTPY2)
+        self.password = 'wrong_password'
+        resp = self.upload(event_list)
+        self.assertEqual(resp, b'203') # wrong password
 
     def test_put_py2_event(self):
-        # Make sure the python 2 pickle throws UnicodeDecodeError
+        event_list = self.read_pickle(EVENTPY2)
+        # Make sure the eventlist is indeed pickled on Python 2
         with self.assertRaises(UnicodeDecodeError):
-            pickle.loads(self.pickled_eventlist_py2)
+            pickle.loads(event_list)
 
-        r_text = self.uploader.upload_data(self.pickled_eventlist_py2)
-        self.assertEqual(r_text, '100') # OK
+        resp = self.upload(event_list)
+        self.assertEqual(resp, b'100')
 
         # verify the event in /incoming
 
     def test_put_py3_event(self):
+        event_list = self.read_pickle(EVENTPY3)
         # Make sure the python 3 pickle is indeed pickled on Python 3
         try:
-            pickle.loads(self.pickled_eventlist_py3)
+            pickle.loads(event_list)
         except UnicodeDecodeError:
             self.fail('Data does not seem pickled on python 3')
 
-        r_text = self.uploader.upload_data(self.pickled_eventlist_py3)
-        self.assertEqual(r_text, '100') # OK
+        resp = self.upload(event_list)
+        self.assertEqual(resp, b'100')
 
         # verify the event in /incoming
 
     def test_put_suspicious_event(self):
-        r_text = self.uploader.upload_data(self.suspicious_eventlist)
-        self.assertEqual(r_text, '100') # OK
+        event_list = self.read_pickle(EVENTSUS)
+        resp = self.upload(event_list)
+        self.assertEqual(resp, b'100')
 
         # verify the event in /suspicious
+
+    def upload(self, pickled_data, checksum=None):
+        """POST. Return response"""
+
+        if checksum is None:
+            checksum = hashlib.md5(pickled_data).hexdigest()
+
+        data = {'station_id': self.station_id,
+                'password': self.password,
+                'data': pickled_data.decode('latin-1'),
+                'checksum': checksum}
+
+        response = self.app.post('/', data)
+        return response.body
+
+    def read_pickle(self, fn):
+        with open(fn, 'rb') as f:
+            pickle = f.read()
+        return pickle
 
 
 if __name__ == '__main__':
