43aa7567f537e4915e98c6a9d9544726b4d2a069
Tom Kooij <tomkooij@tomkooij.nl>
2017-02-26 19:26:03 +0100

    Add acceptance tests for the writer.
    Push different events (event, weather, config, singles) into the writer.
    Test unpacking/decoding etc in Python 3. Can be used to compaire against python 2.

---
+++ tests/test_writer_acceptance.py
@@ -0,0 +1,123 @@
+"""
+Acceptance tests for the writer
+
+python 3
+"""
+import unittest
+import configparser
+import shutil
+import sys
+import tables
+
+from numpy import array
+from numpy.testing import assert_array_equal
+
+# configuration:
+CONFIGFILE = 'test_data/config.ini'
+WRITER_PATH = '../writer'
+DATASTORE_PATH = 'fake_datastore'
+
+STATION_ID = 99
+CLUSTER = 'amsterdam'
+
+CIC_PY2 = 'test_data/incoming_writer/writer_py2_CIC'
+SIN_PY2 = 'test_data/incoming_writer/writer_py2_SIN'
+WTR_PY2 = 'test_data/incoming_writer/writer_py2_WTR'
+CFG_PY2 = 'test_data/incoming_writer/writer_py2_CFG'
+
+
+def import_writer():
+    """import the writer"""
+    sys.path.append(WRITER_PATH)
+    import writer
+    writer.config = configparser.ConfigParser()
+    writer.config.read(CONFIGFILE)
+    return writer
+
+
+def get_writer(writer=import_writer()):
+    """return the WSGI application"""
+    return writer
+
+
+class TestWriterAcceptance(unittest.TestCase):
+
+    def setUp(self):
+        self.writer = get_writer()
+        self.station_id = STATION_ID
+        self.cluster = CLUSTER
+        self.filename = '2017_2_26.h5'
+
+    def tearDown(self):
+        self.clean_datastore()
+
+    def test_event_acceptance(self):
+        self.writer.process_data(CIC_PY2)
+
+        data = self.read_table('events')
+        self.assertEqual(data['timestamp'], 1488093964)
+        self.assertEqual(data['nanoseconds'], 130245552)
+        assert_array_equal(data['pulseheights'], array([[625, 972, 923, 724]]))
+
+        blobs = self.read_table('blobs')
+        self.assertEqual(len(blobs), 4)
+
+    def test_singles_acceptance(self):
+        self.writer.process_data(SIN_PY2)
+
+        data = self.read_table('singles')
+        self.assertEqual(data['timestamp'], 1488094031)
+        self.assertEqual(data['mas_ch1_low'], 345)
+        self.assertEqual(data['mas_ch1_high'], 98)
+        self.assertEqual(data['slv_ch1_low'], 329)
+        self.assertEqual(data['slv_ch1_high'], 95)
+
+        blobs = self.read_table('blobs')
+        self.assertEqual(len(blobs), 0)
+        self.fail('Unpack blobs!')
+
+    def test_weather_acceptance(self):
+        self.writer.process_data(WTR_PY2)
+
+        data = self.read_table('weather')
+        self.assertEqual(data['timestamp'], 1488094084)
+        self.assertEqual(data['uv'], -999)
+        self.assertAlmostEqual(data['barometer'], 1004.365)
+        self.assertEqual(data['humidity_inside'], 25)
+
+        blobs = self.read_table('blobs')
+        self.assertEqual(len(blobs), 0)
+
+    def test_config_acceptance(self):
+        self.writer.process_data(CFG_PY2)
+        data = self.read_table('config')
+        self.assertEqual(data['timestamp'], 1488125225)
+        self.assertEqual(data['mas_ch1_thres_high'], 320)
+        self.assertEqual(data['mas_ch1_thres_low'], 250)
+        self.assertEqual(data['reduce_data'], False)
+        self.assertAlmostEqual(data['mas_ch1_voltage'], 300)
+        self.assertAlmostEqual(data['slv_ch1_current'], 0.0)
+
+        blobs = self.read_table('blobs')
+        self.assertEqual(len(blobs), 2)
+        self.assertEqual(blobs[0], b'')
+        self.assertEqual(blobs[1], b'Hardware: 0 FPGA: 0')
+
+    def read_table(self, table):
+        year, month, _ = self.filename.split('_')
+        path = DATASTORE_PATH+'/'+year+'/'+month+'/'+self.filename
+        table_path = '/hisparc/cluster_%s/station_%s/%s' % (self.cluster,
+                                                            self.station_id,
+                                                            table)
+        with tables.open_file(path, 'r') as datafile:
+            t = datafile.get_node(table_path)
+            data = t.read()
+
+        return data
+
+    def clean_datastore(self):
+        shutil.rmtree(DATASTORE_PATH+'/2017')
+
+
+if __name__ == '__main__':
+    unittest.main()
