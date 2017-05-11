63e4b67e40f849b3c82c1243bff797c33a8f4498
Tom Kooij <tomkooij@tomkooij.nl>
2017-05-11 09:22:26 +0200

    Change writer.py to writer_app.py (writer package)
    Prevent ambiguous imports: writer is a package. The writer application is in
    the writer_app module of the writer package. The `main` function is named
    `writer`.
    
    from writer import writer_app
    writer_app.writer()
    
    writer/
      __init__.py
      writer_app.py
      (modules)

--- writer/__init__.py
+++ writer/__init__.py
@@ -1,12 +1,2 @@
 """ HDF5 writer application
 """
-
-from . import writer
-from . import store_events
-from . import storage
-from . import upload_codes
-
-__all__ = ['writer',
-           'store_events',
-           'storage',
-           'upload_codes']
