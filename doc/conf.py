29e9b3827af034e5e68d9b9d2504a6c7b10a8da8
Tom Kooij <tomkooij@tomkooij.nl>
2017-05-11 10:05:34 +0200

    Set the default HiSPARC style
    Add hisparc_style.css
    Favicon
    Logo's

--- doc/conf.py
+++ doc/conf.py
@@ -86,13 +86,22 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 # html_theme_options = {}
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = 'logo/header.png'
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = '_static/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -105,6 +114,9 @@ html_static_path = ['_static']
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'HiSPARC-datastoredoc'
 
+
+def setup(app):
+    app.add_stylesheet('hisparc_style.css')
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -133,6 +145,11 @@ latex_documents = [
     (master_doc, 'HiSPARC-datastore.tex', 'HiSPARC-datastore Documentation',
      'David Fokkema, Arne de Laat, Tom Kooij', 'manual'),
 ]
+
+# The name of an image file (relative to this directory) to place at the top of
+# the title page.
+latex_logo = 'logo/logo.pdf'
+
 
 
 # -- Options for manual page output ---------------------------------------
