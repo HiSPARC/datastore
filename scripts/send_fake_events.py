55161d02dd2bb99d270d1d00cdbd9362d1794328
Tom Kooij <tomkooij@tomkooij.nl>
2019-04-08 10:28:54 +0200

    send_fake_events: test dates around Apr 7, 2019

--- scripts/send_fake_events.py
+++ scripts/send_fake_events.py
@@ -1,26 +1,29 @@
 import datetime
 import random
-import time
 
 import numpy as np
 
 import pysparc.events
 import pysparc.storage
 
+from sapphire import datetime_to_gps
+
 
 class FakeMessage(object):
 
-    datetime = datetime.datetime.now()
-    timestamp = time.time()
-    nanoseconds = int(random.uniform(0, 1e9))
-    ext_timestamp = int(timestamp) * int(1e9) + nanoseconds
-    trigger_pattern = 1
-    trace_ch1 = np.arange(10)
-    trace_ch2 = np.arange(10)
+    def __init__(self, dt=datetime.datetime.now()):
+        self.datetime = dt
+        self.timestamp = datetime_to_gps(dt)
+        self.nanoseconds = int(random.uniform(0, 1e9))
+        self.ext_timestamp = int(self.timestamp) * int(1e9) + self.nanoseconds
+        self.trigger_pattern = 1
+        self.trace_ch1 = np.arange(10)
+        self.trace_ch2 = np.arange(10)
 
 
-datastore = pysparc.storage.NikhefDataStore(99, 'fake_station',
-                                            url='http://localhost:8083')
-for i in range(100):
-    event = pysparc.events.Event(FakeMessage())
+VM = 'http://localhost:8083/nikhef/upload'
+datastore = pysparc.storage.NikhefDataStore(99, 'fake_station', url=VM)
+
+for d in range(1, 10):
+    event = pysparc.events.Event(FakeMessage(dt=datetime.datetime(2019, 4, d)))
     datastore.store_event(event)
